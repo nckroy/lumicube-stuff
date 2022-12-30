fe2a9dcafed0fd142bf86c1fd1d812002f117e96
Nicole Roy <nicolesroy@icloud.com>
2022-12-30 16:36:30 -0700

    Improved timing

--- cm1-blinkenlights.py
+++ cm1-blinkenlights.py
@@ -22,10 +22,10 @@ while True:
             tf = random.getrandbits(1)
             if tf:
                 art[x][y] = r
-                time.sleep(1/80)
     if panel == 0:
         display.set_panel('top', art)
     elif panel == 1:
         display.set_panel('left', art)
     elif panel == 2:
         display.set_panel('right', art)
+    time.sleep(1/10)
