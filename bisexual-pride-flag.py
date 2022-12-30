877eeaeb6b20cdab793bec63440bca577c05a9c3
Nicole Roy <nicolesroy@icloud.com>
2022-12-30 13:09:58 -0700

    First commit

---
+++ bisexual-pride-flag.py
@@ -0,0 +1,42 @@
+# Bisexual pride procedural thinger for lumicube, written (terribly) by Nicole Roy, 2022
+
+import colorsys
+
+def lava_colour(x, y, z, t):
+    scale = 0.10
+    speed = 0.05
+    hue = noise_4d(scale * x, scale * y, scale * z, speed * t)
+    rgb = list(colorsys.hsv_to_rgb(hue, 1, 1))
+    rgb[1] = -1
+    while rgb[0] <= -1 or rgb[0] >= 1:
+        if rgb[0] <=-1:
+            rgb[0] += random.uniform(0.01, 0.5)
+        if rgb[0] >= 1:
+            rgb[0] -= random.uniform(0.01, 0.5)
+    try:
+        hsv = list(colorsys.rgb_to_hsv(rgb[0], rgb[1], rgb[2]))
+    except:
+        hsv = list(colorsys.rgb_to_hsv(-0.2, 0, 1))
+    hsvcol = list(colorsys.rgb_to_hsv(-0.2, 0, 1))
+    try:
+        hsvcol = list(colorsys.rgb_to_hsv(rgb[0], rgb[1], rgb[2]))
+        return hsv_colour(hsv[0], hsv[1], hsv[2])
+    except:
+        return hsv_colour(hsv[0], hsv[1], hsv[2])
+
+def paint_cube(t):
+    colours = {}
+    for x in range(9):
+        for y in range(9):
+            for z in range(9):
+                if x == 8 or y == 8 or z == 8:
+                    colour = lava_colour(x, y, z, t)
+                    colours[x,y,z] = colour
+    display.set_3d(colours)
+
+t = 0
+while True:
+    paint_cube(t)
+    time.sleep(1/30)
+    t += 1
+
